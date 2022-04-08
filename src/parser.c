a48cb12cdf148a508f9edd5468a57fc16765e3bd
gradagas <gradagas@student.21-school.ru>
2022-04-08 20:28:34 +0300

    fix-map_parser

--- src/parser.c
+++ src/parser.c
@@ -22,23 +22,25 @@ char	*get_map_in_line(char *map_file)
 	if (fd == -1)
 		error1("Error : can't open file\n");
 
-	buff = (char *) malloc(sizeof(char) * BUFF_SIZE);
+	buff = (char *) malloc(sizeof(char) * (BUFF_SIZE + 1));
 	if (!buff)
 		error1("Error : malloc failed\n");
 
-	ft_bzero(buff, BUFF_SIZE);
 	was_read = read(fd, buff, BUFF_SIZE);
 	if (was_read == -1)
 		error1("Error : read file failed\n");
+	buff[was_read] = '\0';
 	map_in_line = ft_strdup(buff);
 	while (was_read == BUFF_SIZE)
 	{
-		ft_bzero(buff, BUFF_SIZE);
+//		ft_bzero(buff, BUFF_SIZE);
 		was_read = read(fd, buff, BUFF_SIZE);
 		if (was_read == -1)
 			error1("Error : read file failed\n");
+		buff[was_read] = '\0';
 		tmp = map_in_line;
 		map_in_line = ft_strjoin(map_in_line, buff);
+//		map_in_line = ft_substr(buff, 0, was_read);
 		free(tmp);
 	}
 	free(buff);
