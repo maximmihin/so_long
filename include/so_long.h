ea59c7ad7ff50da6c707ba9a93ebc43c01d53fd1
gradagas <gradagas@student.21-school.ru>
2022-05-06 20:28:16 +0300

    add-start-rendering

--- include/so_long.h
+++ include/so_long.h
@@ -9,6 +9,13 @@
 #  define BUFF_SIZE 42
 # endif
 
+#define SCALE 150
+#define EMPTY_SPACE "../textures/empty_space.xpm"
+#define WALL "../textures/wall.xpm"
+#define COLLECTIBLE "../textures/collectible.xpm"
+#define EXIT "../textures/exit.xpm"
+#define PLAYER "../textures/player.xpm"
+
 typedef struct s_map_data
 {
 	char	**map;
@@ -19,10 +26,37 @@ typedef struct s_map_data
 	int		collectibles;
 }	t_map_data;
 
+typedef struct s_image_textures
+{
+	void	*empty_space;
+	void	*wall;
+	void	*collectible;
+	void	*exit;
+	void	*player;
+}	t_image_textures;
+
+typedef struct s_render_data
+{
+	void				*mlx;
+	void				*mlx_win;
+	char				*addr;
+	int					bits_per_pixel;
+	int					line_length;
+	int					endian;
+	t_image_textures	image_textures;
+}	t_render_data;
+
+
+
+
 void	error1(char *str);
 
 char		**map_parser(char *map_file);
 void		map_validation(char const **map);
 t_map_data	extract_map_data(t_map_data map_data);
+
+t_image_textures	init_textures(void	*mlx);
+void 				render_map(void *mlx, void *mlx_win, char **map,
+								t_image_textures image_textures);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -16,7 +16,8 @@ void	print_map(char **map)
 
 int main(int argc, char **argv)
 {
-	t_map_data map_data;
+	t_map_data		map_data;
+	t_render_data	render_data;
 
 	if (argc != 2)
 		error1("Error : num arguments must be 1\n");
@@ -25,5 +26,16 @@ int main(int argc, char **argv)
 	map_validation((const char **)map_data.map);
 	map_data = extract_map_data(map_data);
 	print_map(map_data.map);
+
+	render_data.mlx = mlx_init();
+	render_data.mlx_win = mlx_new_window(render_data.mlx, SCALE * map_data.map_width,
+							 SCALE * map_data.map_height, "so_long");
+
+	render_data.image_textures = init_textures(render_data.mlx);
+	render_map(render_data.mlx, render_data.mlx_win, map_data.map,
+			   render_data.image_textures);
+
+	mlx_loop(render_data.mlx);
+
 	return (0);
 }
